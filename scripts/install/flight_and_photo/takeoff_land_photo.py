8ed9b70f2d916a9e2997dfd65a138591a1c5bb45
IgorLebed <hazz808@gmail.com>
2019-04-25 11:49:55 +0000

    add scripts

---
+++ scripts/install/flight_and_photo/takeoff_land_photo.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+from time import time
+from time import sleep
+from flyt_python import api
+import cv2
+import pexif
+import sys, os
+
+drone = api.navigation(timeout=120000)  # instance of flyt droneigation class
+# at least 3sec sleep time for the drone interface to initialize properly
+sleep(3)
+
+cam = "http://localhost:8080/stream?topic=/flytsim/flytcam/image_capture&width=320&height=240&type=ros_compressed"
+stream = cv2.VideoCapture(cam)
+RUN = True
+last_time = time()
+img_counter = 0
+
+
+#send gimbal attitude set
+drone.gimbal_control(0, -1.5, 0)
+
+print 'taking off'
+drone.take_off(5.0)
+sleep(5)
+
+# Poll data
+gpos = drone.get_global_position()
+# Print the data
+print gpos.lat, gpos.lon, gpos.alt
+
+
+def make_photo(last_time, now):
+    if now - last_time > 5:
+        return True
+while RUN:
+    ret, img = stream.read()
+    now = time()
+    if make_photo(last_time, now):
+        f_name = str(img_counter) + ".jpg"
+        cv2.imwrite(f_name, img)
+        pose_v = drone.get_global_position()
+        la = pose_v.lat
+        lo = pose_v.lon
+        al = pose_v.alt
+        img_set = pexif.JpegFile.fromFile(str(img_counter) + ".jpg")
+        img_set.set_geo(float(la), float(lo), float(al))
+        #path = '/flyt/userapps/onboard/install/flight_and_photo/photo'
+        img_set.writeFile(str(img_counter) + "_with_geo" + ".jpg")
+        img_counter += 1
+        last_time = now
+        print(f_name + " has been saved at " + str(now))
+    if img_counter == 5:
+    #if (vehicle.mode == VehicleMode("LAND")):
+        RUN = not RUN
+
+# Poll data
+gpos = drone.get_global_position()
+# Print the data
+print gpos.lat, gpos.lon, gpos.alt
+
+#print ' going along the setpoints'
+#drone.position_set(5, 0, 0, relative=True)
+#drone.position_set(0, 5, 0, relative=True)
+#drone.position_set(-5, 0, 0, relative=True)
+#drone.position_set(0, -5, 0, relative=True)
+
+print 'Landing'
+drone.land(async=False)
+
+# shutdown the instance
+drone.disconnect()
