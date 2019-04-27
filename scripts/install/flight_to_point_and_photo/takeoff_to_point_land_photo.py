5cf4a48404dee72086e43b07d7ba975f7b484834
IgorLebed <hazz808@gmail.com>
2019-04-27 16:19:49 +0000

    add photo and change script

--- scripts/install/flight_to_point_and_photo/takeoff_to_point_land_photo.py
+++ scripts/install/flight_to_point_and_photo/takeoff_to_point_land_photo.py
@@ -4,21 +4,23 @@ from time import sleep
 from flyt_python import api
 import cv2
 import pexif
-import os
+import sys, os
+from threading import Thread
 
 drone = api.navigation(timeout=120000)  # instance of flyt droneigation class
 # at least 3sec sleep time for the drone interface to initialize properly
-sleep(3)
+#sleep(3)
 
 #send gimbal attitude set
 drone.gimbal_control(0, -1.5, 0)
 
 # Poll data
-gpos = drone.get_global_position()
+#gpos = drone.get_global_position()
 # Print the data
-print gpos.lat, gpos.lon, gpos.alt
+#print gpos.lat, gpos.lon, gpos.alt
 
 def photo_pose():
+    #cam = 0
     cam = "http://localhost:8080/stream?topic=/flytsim/flytcam/" \
           "image_capture&width=320&height=240&type=ros_compressed"
     stream = cv2.VideoCapture(cam)
@@ -36,25 +38,26 @@ def photo_pose():
         if make_photo(last_time, now):
             f_name = str(img_counter) + ".jpg"
             cv2.imwrite(os.path.join(path, f_name), img)
-            pose_v = drone.get_global_position()
-            la = pose_v.lat
-            lo = pose_v.lon
-            al = pose_v.alt
-            img_set = pexif.JpegFile.fromFile("/flyt/userapps/onboard_user/install/"
-                          "flight_to_point_and_photo/photo/" +  str(img_counter) + ".jpg")
-            img_set.set_geo(float(la), float(lo), float(al))
-            img_set.writeFile("/flyt/userapps/onboard_user/install/"
-                              "flight_to_point_and_photo/"
-                              "photo_with_geo/" + str(img_counter) + "_with_geo" + ".jpg")
+            #pose_v = drone.get_global_position()
+            #la = pose_v.lat
+            #lo = pose_v.lon
+            #al = pose_v.alt
+            #img_set = pexif.JpegFile.fromFile("/flyt/userapps/onboard_user/install/"
+             #             "flight_to_point_and_photo/photo/" +  str(img_counter) + ".jpg")
+            #img_set.set_geo(float(la), float(lo), float(al))
+            #img_set.writeFile("/flyt/userapps/onboard_user/install/"
+            #                  "flight_to_point_and_photo/"
+            #                  "photo_with_geo/" + str(img_counter) + "_with_geo" + ".jpg")
             img_counter += 1
             last_time = now
             print(f_name + " has been saved at " + str(now))
-        if img_counter == 30:
+        if img_counter == 10:
             RUN = not RUN
 
 
 def flight2point():
-    print 'taking off'
+    #drone = api.navigation(timeout=120000) 
+    print "taking off"
     drone.take_off(5.0)
     #sleep(5)
 
@@ -71,7 +74,7 @@ def flight2point():
     drone.disconnect()
 
 
-th_1, th_2 = Thread(target = fligt2point), Thread(target = photo_pose)
+th_1, th_2 = Thread(target = flight2point), Thread(target = photo_pose)
 if __name__ == '__main__':
     th_1.start(), th_2.start()
     th_1.join(), th_2.join()
