4b3cb27f2332980860972a67bc39e7832e74901a
IgorLebed <hazz808@gmail.com>
2019-10-23 08:13:53 +0000

    new commit

---
+++ catkin_ws/build/ros_demoapps/CMakeFiles/att_check_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/att_check_node.dir/src/att_check.cpp.o"
+  "/root/catkin_ws/devel/lib/ros_demoapps/att_check_node.pdb"
+  "/root/catkin_ws/devel/lib/ros_demoapps/att_check_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/att_check_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws/build/ros_demoapps/CMakeFiles/emergency_land_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/emergency_land_node.dir/src/emergency_battery.cpp.o"
+  "/root/catkin_ws/devel/lib/ros_demoapps/emergency_land_node.pdb"
+  "/root/catkin_ws/devel/lib/ros_demoapps/emergency_land_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/emergency_land_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
