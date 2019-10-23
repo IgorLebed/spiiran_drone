d104d9bdfd8e33ad1df4e022c8750e945553b10a
IgorLebed <hazz808@gmail.com>
2019-10-23 13:09:40 +0300

    new commit

---
+++ ros/ros_drone_task/src/battery_check.cpp
@@ -0,0 +1,24 @@
+#include <ros/ros.h>
+#include <sensor_msgs/BatteryState.h>
+
+//sensor_msgs::BatteryState battery_date;
+std::string global_namespace;
+
+void battery_callback(const sensor_msgs::BatteryStateConstPtr &battery)
+{
+  std::cout << "\nVoltage \tcurrent";
+  std::cout << "\n" << battery->voltage << "\t\t" << battery->current;
+  //float x = battery->voltage;
+  //if (x < 20){
+  //  ROS_INFO("FAIL BATTERY");
+  //}
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "battery_callback");
+  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("/"+global_namespace+"/mavros/battery", 1000, battery_callback);
+  ros::spin();
+  return 0;
+}
