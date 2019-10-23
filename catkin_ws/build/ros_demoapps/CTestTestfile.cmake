23e2cda64de35a2d7233c8461c75dfca09965a2d
IgorLebed <hazz808@gmail.com>
2019-10-23 08:16:53 +0000

    new commit

--- catkin_ws/build/ros_demoapps/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/catkin_ws/src/ros_demoapps
-# Build directory: /root/catkin_ws/build/ros_demoapps
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
